f7296f5b1c9666ea2a9bdf18d6b8c97a482e1503
Juliana Ramirez <ramirezjuliana834@gmail.com>
2020-03-12 23:26:23 -0500

    algoritmo 5

---
+++ algotimo_5.js
@@ -0,0 +1,17 @@
+A=parseFloat(prompt("introduce el primer numero"));
+
+B=parseFloat(prompt("introduce el segundo numero"));
+
+C=parseFloat(prompt("introduce el tercer numero"));
+
+if (A<0) {
+
+resultado=A*B*C;
+
+}
+
+else{
+
+      resultado=A+B+C;
+
+}
