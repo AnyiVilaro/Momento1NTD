132b4f70b65f9f24db83aa5f5730b26199a420f2
Juliana Ramirez <ramirezjuliana834@gmail.com>
2020-03-12 23:27:22 -0500

    algoritmo 5

---
+++ algotimo_6.js
@@ -0,0 +1,19 @@
+a=parseFloat(prompt("introduce un numero"));
+
+if(a>0){
+
+      potencia=Math.pow(a, 2);
+
+      raiz=Math.sqrt(a);
+
+      alert("su potencia es "+potencia );
+
+      alert("su raiz es "+raiz);
+
+}
+
+else{
+
+      alert("error introduce un numero mayor que 0");
+
+}
