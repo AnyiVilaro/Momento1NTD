7057fdd85a0d31e3fe56eb9f1655e2071f13c2aa
Juliana Ramirez <ramirezjuliana834@gmail.com>
2020-03-12 23:19:34 -0500

    Algoritmo 4

---
+++ algotimo_4.js
@@ -0,0 +1,29 @@
+
+
+a=parseFloat(prompt("introduce el primer numero"));
+
+b=parseFloat(prompt("introduce el segundo numero"));
+
+c=parseFloat(prompt("introduce el tercer numero"));
+
+if(a>b && a>c){
+
+alert("el numero " +a+  "es el mayor")
+
+}
+
+else{
+
+      if (b>c) {
+
+alert("el numero "+b+ " es el mayor")
+
+}
+
+else{
+
+alert("el numero "+c+" es el mayor")
+
+}
+
+}
