906bc4a733116372bcf286e21b7367d1deebd049
Anyi Vilaró <anmivi15@gmail.com>
2020-03-12 23:05:40 -0500

    Algoritmo 3

---
+++ algoritmo3.js
@@ -0,0 +1,27 @@
+var a = parseInt(prompt("Ingrese número 1: ",0));
+
+var b = parseInt(prompt("Ingrese número 2: ",0));
+
+ 
+
+if (a < b){
+
+                console.log("A:" + a + " es menor que B: " + b)
+
+}
+
+ 
+
+if (a > b){
+
+                console.log("A:" + a + " es mayor que B: " + b)
+
+}
+
+ 
+
+if (a == b){
+
+                console.log("A:" + a + " es igual a B: " + b)
+
+}
