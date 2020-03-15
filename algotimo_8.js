893fd6d5eca99eadfa470d681cea7ab97c2ac262
Alejandro Echavarria <alejandroechavarriacano@gmail.com>
2020-03-15 15:14:38 -0500

    algoritmos 7, 8, 9, 10

---
+++ algotimo_8.js
@@ -0,0 +1,19 @@
+importe=parseFloat(prompt("Escribe el importe de la compra"));
+
+mes=prompt("Digite el mes");
+
+if(mes=="octubre"){
+
+total=importe*0.85;
+
+alert("El total es: "+total);
+
+}
+
+else{
+
+      total=importe;
+
+      alert("El valor total es: "+total);
+
+}
---
+++ algotimo_9.js
@@ -0,0 +1,23 @@
+A=parseFloat(prompt("Introduce un numero"));
+
+if(A<=0){
+
+      alert("El "+A+" no es par ni impar")
+
+}
+
+else{
+
+      if(A%2==0){
+
+                     alert("El "+A+" es par");
+
+      }
+
+      else{
+
+                     alert("El "+A+" es impar");
+
+      }
+
+}
